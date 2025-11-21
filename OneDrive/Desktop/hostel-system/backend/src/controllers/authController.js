edbe192a8273e93299592aa065da0d6fd1859941
Ananyank-08 <ananyank8@gmail.com>
2025-11-21 10:49:12 +0530

    backend folder added

---
+++ OneDrive/Desktop/hostel-system/backend/src/controllers/authController.js
@@ -0,0 +1,42 @@
+const bcrypt = require('bcrypt');
+const pool = require('../db');
+const { signToken } = require('../utils/jwt');
+
+exports.register = async (req, res) => {
+  const { name, email, password, role } = req.body;
+
+  try {
+    const hashed = await bcrypt.hash(password, 10);
+
+    await pool.query(
+      "INSERT INTO users (name,email,password,role) VALUES (?,?,?,?)",
+      [name, email, hashed, role]
+    );
+
+    res.json({ message: "Registered" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const [rows] = await pool.query("SELECT * FROM users WHERE email=?", [email]);
+
+    if (!rows.length) return res.status(401).json({ error: "Invalid email" });
+
+    const user = rows[0];
+
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid) return res.status(401).json({ error: "Invalid password" });
+
+    const token = signToken({ id: user.id, role: user.role });
+
+    res.json({ token, user: { id: user.id, name: user.name, role: user.role } });
+
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
